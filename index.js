fc9e2a2f177e58f0f8386fc95b55d0a6479e7de7
j-frilot <frilotdr@gmail.com>
2021-05-13 15:12:13 -0500

    second commit

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ app.use(cors());
 // Port Environment variable
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World!!!!!!!!!!!!!!!!!!!!</h1>");
+});
+
 // parse incoming traditional HTML form submits
 app.use(express.urlencoded({ extended: false }));
 
