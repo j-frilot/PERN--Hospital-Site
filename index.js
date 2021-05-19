cf537fb4d107609e282cdab4d526390b6f3a27b5
j-frilot <frilotdr@gmail.com>
2021-05-19 13:51:09 -0500

    update

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-const path = require("path");
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const helmet = require("helmet");
 const router = require("./router");
+const helmet = require("helmet");
+const cors = require("cors");
+const path = require("path");
 
 app.use(helmet());
 app.use(cors());
+app.use(express.static("public"));
 
-// Port Environment variable
-const PORT = process.env.PORT || 5000;
-
-// app.get("/", (req, res) => {
-//     res.send("<h1>Hello World!!!!!!!!!!!!!!!!!!!!</h1>");
-// });
-// commented out
+app.use("/api", router);
 
 // parse incoming traditional HTML form submits
 app.use(express.urlencoded({ extended: false }));
@@ -22,16 +17,13 @@ app.use(express.urlencoded({ extended: false }));
 // parse incoming JSON payloads
 app.use(express.json());
 
-app.use("/api", router);
-app.use(express.static("public"));
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5000");
+    next();
+});
 
-// **deploy react**
-
-// app.use(express.static(path.join(__dirname, "build")));
-// app.get("/*", (req, res) => {
-//     res.sendFiles(path.join(__dirname, "build", index.html));
-// });
-// **deploy react **
+// Port Environment variable
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
