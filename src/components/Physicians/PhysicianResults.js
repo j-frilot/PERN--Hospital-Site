56b41da784035bd9b5d26fc1ebde5364bb0b8e30
j-frilot <frilotdr@gmail.com>
2021-06-28 12:19:21 -0500

    removed images, getting from cdn

--- src/components/Physicians/PhysicianResults.js
+++ src/components/Physicians/PhysicianResults.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
-const PhysicianResults = () => {
+const PhysicianResults = (props) => {
     const [allDoctors, setAllDoctors] = useState([]);
     const [inputBar, setInputBar] = useState("");
     const [filteredDoctor, setFilteredDoctor] = useState([]);
