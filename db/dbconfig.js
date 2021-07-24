a6127773cd7e0c1ace1a811c464ee53c16f84495
j-frilot <frilotdr@gmail.com>
2021-07-23 21:47:54 -0500

    integrating routes

--- db/dbconfig.js
+++ db/dbconfig.js
@@ -1,24 +1,24 @@
 require("dotenv").config();
-const { Pool, Client } = require("pg");
+const { Pool } = require("pg");
 
-// // create the connection to database
-// const conn = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     database: process.env.DB_NAME
-// });
+// // // create the connection to database
+// // const conn = mysql.createConnection({
+// //     host: process.env.DB_HOST,
+// //     user: process.env.DB_USER,
+// //     password: process.env.DB_PASS,
+// //     database: process.env.DB_NAME
+// // });
 
-const pool = new Pool();
+// const pool = ne  w Pool();
 
-// const pool = new Pool({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASS,
-//     database: process.env.DB_NAME,
-//     port: process.env.DB_PORT,
-//     connectionLimit: 100
-// });
+const pool = new Pool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    connectionLimit: 100
+});
 
 pool.query("SELECT NOW()", (err, res) => {
     console.log("Oh yea, our connection is established!: ");
--- routes/physicianRoutes.js
+++ routes/physicianRoutes.js
@@ -1,21 +1,36 @@
 const express = require("express");
 const router = express.Router();
-const { physicianQueries: query } = require("../queries/Query");
+// const { physicianQueries: query } = require("../queries/Query");
+const db = require("../db/dbconfig");
 
 //    3000/api/physicians
 
 // all of the physicians  :3000/api/physicians
-router.get("/", (req, res) => {
-    query.findAll(res, query.table);
+router.get("/", async (req, res) => {
+    const allPhysicians = await db.query("SELECT * FROM physicians");
+    console.log(allPhysicians);
+
+    // query.findAll(res, query.table);
 });
 
 // find by id  ${PORT}api/physicians/5
-router.get("/:id", (req, res) => {
-    query.findById(res, query.table, req.params.id);
+router.get("/:id", async (req, res) => {
+    const physicianId = await db.query(
+        `SELECT * FROM physicians WHERE physicians_id = ?`,
+        [id]
+    );
+    console.log(physicianId);
+    // query.findById(res, query.table, req.params.id);
 });
 
 // filter names  ${PORT/api/physicians/filter/'letters'
-router.get("/filter/:str", (req, res) => {
-    query.filterName(res, req.params.str);
+router.get("/filter/:str", async (req, res) => {
+    const filter = await db.query(
+        `SELECT *  FROM physicians WHERE (first_name LIKE CONCAT('%', ? , '%')) OR (last_name LIKE CONCAT('%', ? , '%'))`,
+        [str, str]
+    );
+    console.log(filter);
+
+    // query.filterName(res, req.params.str);
 });
 module.exports = router;
